4ba52cb1e566d52efcde02261ed5e2ffdf0b3255
oiffice <oiffice90121@gmail.com>
2022-10-20 16:14:05 +0800

    Init

---
+++ src/test/java/com/example/hackerrank/HackerrankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackerrank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerrankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
